25a305a3bb2c48163e1bc6d573cb761ec0987966
chamika-damith <chamikadamith9@gmail.com>
2023-11-03 09:02:35 +0530

    updated

---
+++ src/main/java/lk/ijse/LauncherWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane LoginRoot;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent= FXMLLoader.load(this.getClass().getResource("/view/Dashboard/DashboardForm.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage= (Stage) LoginRoot.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
+    }
 }
