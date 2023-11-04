a626160fbc5facbd36535f74c87b6c3aba0b3b59
chamika-damith <chamikadamith9@gmail.com>
2023-11-04 22:59:22 +0530

    updated

--- src/main/java/lk/ijse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+    public AnchorPane DashboardRoot;
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent= FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage= (Stage) DashboardRoot.getScene().getWindow();
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+    }
 }
