1f1103579e3cb8b1ed5c5e5f49e034cb01b93d8f
chamika-damith <chamikadamith9@gmail.com>
2023-11-09 03:53:57 +0530

    Updated

--- src/main/java/lk/ijse/controller/Inventory/InventoryFormController.java
+++ src/main/java/lk/ijse/controller/Inventory/InventoryFormController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class InventoryFormController {
 
@@ -15,9 +20,22 @@ public class InventoryFormController {
     public JFXTextField price;
     public JFXTextField description;
     public JFXComboBox comboBox;
+
+    public Image image;
+
+    public FileInputStream fileInputStream;
     public ImageView img;
+    public JFXButton imgSelectBtn;
 
     public void btnImgSelect(ActionEvent actionEvent) {
-
+            FileChooser chooser = new FileChooser();
+            File file =chooser.showOpenDialog(imgSelectBtn.getScene().getWindow());
+            try {
+                fileInputStream=new FileInputStream(file);
+                image=new Image(fileInputStream);
+                img.setImage(image);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
