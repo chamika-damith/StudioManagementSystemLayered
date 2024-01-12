e4b9fb11b9697accf2ef228361c87576d633ce6b
chamika-damith <chamikadamith9@gmail.com>
2024-01-12 15:00:07 +0530

    updated

--- src/main/java/lk/ijse/bo/custom/impl/OrderBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/OrderBOImpl.java
@@ -1,6 +1,9 @@
 package lk.ijse.bo.custom.impl;
 
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.ItemBO;
 import lk.ijse.bo.custom.OrderBO;
+import lk.ijse.bo.custom.OrderDetailBO;
 import lk.ijse.dao.DAOFactory;
 import lk.ijse.dao.custom.ItemDAO;
 import lk.ijse.dao.custom.OrderDAO;
@@ -16,28 +19,30 @@ public class OrderBOImpl implements OrderBO {
 
     private OrderDAO orderDAO= (OrderDAO) DAOFactory.getFactory().getDao(DAOFactory.DADTypes.ORDER);
 
-    private ItemDAO itemDAO= (ItemDAO) DAOFactory.getFactory().getDao(DAOFactory.DADTypes.ITEM);
+    private OrderBO orderBO= (OrderBO) BOFactory.getFactory().getBO(BOFactory.BOTypes.ORDER);
 
-    private OrderDetailDAO orderDetailDAO= (OrderDetailDAO) DAOFactory.getFactory().getDao(DAOFactory.DADTypes.ORDERDETAIL);
+    private ItemBO itemBO= (ItemBO) BOFactory.getFactory().getBO(BOFactory.BOTypes.ITEM);
+
+    private OrderDetailBO orderDetailBO= (OrderDetailBO) BOFactory.getFactory().getBO(BOFactory.BOTypes.ORDERDETAIL);
 
     @Override
     public int generateNextOrderId() throws SQLException, ClassNotFoundException {
-        return orderDAO.generateNextOrderId();
+        return orderBO.generateNextOrderId();
     }
 
     @Override
     public boolean isExists(int id) throws SQLException, ClassNotFoundException {
-        return orderDAO.isExists(id);
+        return orderBO.isExists(id);
     }
 
     @Override
     public String returnLbOrderlValue() throws SQLException, ClassNotFoundException {
-        return orderDAO.returnLbOrderlValue();
+        return returnLbOrderlValue();
     }
 
     @Override
     public String returnlblTotalSale() throws SQLException, ClassNotFoundException {
-        return orderDAO.returnlblTotalSale();
+        return orderBO.returnlblTotalSale();
     }
 
     @Override
@@ -52,10 +57,10 @@ public class OrderBOImpl implements OrderBO {
             ,orderDto.getUserId(),orderDto.getCusId(),orderDto.getTotal());
             if (isOrderSave) {
                 System.out.println("order saved");
-                boolean isItemSave = itemDAO.updateItems(orderDto.getCartTmList(),orderDto.getQty());
+                boolean isItemSave = itemBO.updateItems(orderDto.getCartTmList(),orderDto.getQty());
                 System.out.println("item saved");
                 if (isItemSave) {
-                    boolean isOrderDetailSave = orderDetailDAO.saveOrderDetails(orderDto.getOrderId(), orderDto.getCartTmList());
+                    boolean isOrderDetailSave = orderDetailBO.saveOrderDetails(orderDto.getOrderId(), orderDto.getCartTmList());
                     if (isOrderDetailSave){
                         System.out.println("Order details saved");
                         connection.commit();
